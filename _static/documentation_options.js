0232297838f0eb5ac8ad71fbaaa9c4621ce422f3
charliekilpatrick <charliekilpatrick@users.noreply.github.com>
2026-05-07 20:06:02 +0000

    deploy: a7a96b84b4031e5dbf4fec9474823433421c5088

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.0+ga7a96b8.dirty',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
